6b93abe7821ba39794b07c522679dd6045d819fe
covest2051 <maratpetrovitch@gmail.com>
2025-10-09 16:37:16 +0300

    feat(java-core): implement minispring main features

---
+++ src/main/java/javacore/fourth/minispring/Main.java
@@ -0,0 +1,21 @@
+package javacore.fourth.minispring;
+
+import javacore.fourth.minispring.beans.factory.BeanFactory;
+import javacore.fourth.minispring.testEntity.SomeComponent;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException, ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.instantiate("javacore.fourth.minispring.testEntity");
+        beanFactory.populateProperties();
+
+        SomeComponent someComponent = beanFactory.getBean(SomeComponent.class);
+
+        System.out.println(someComponent);
+        System.out.println(someComponent.getBeanToInjectInComponent());
+    }
+}
---
+++ src/main/java/javacore/fourth/minispring/testEntity/SomeComponent.java
@@ -0,0 +1,18 @@
+package javacore.fourth.minispring.testEntity;
+
+import javacore.fourth.minispring.beans.factory.annotation.Autowired;
+import javacore.fourth.minispring.beans.factory.stereotype.Component;
+
+@Component
+public class SomeComponent {
+    @Autowired
+    private BeanToInjectInComponent beanToInjectInComponent;
+
+    public BeanToInjectInComponent getBeanToInjectInComponent() {
+        return beanToInjectInComponent;
+    }
+
+    public void setBeanToInjectInComponent(BeanToInjectInComponent beanToInjectInComponent) {
+        this.beanToInjectInComponent = beanToInjectInComponent;
+    }
+}
